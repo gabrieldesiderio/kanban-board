e1b002b5d664a090637cdd215736e41d94254fcc
Gabriel Desidério <gabrieldesd7@gmail.com>
2024-05-11 20:18:57 -0300

    config: eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
-import './global.css' 
+import './global.css'
 import KanbanBoard from './components/board'
 
 export function App() {
   return (
-    <KanbanBoard />
+    <div>
+      <KanbanBoard />
+    </div>
   )
 }
